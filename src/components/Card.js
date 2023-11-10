0effe724645c20abafaef5f2cd06cdc9a386d97a
Rukky <abrukevwe@gmail.com>
2023-11-10 21:54:32 +0100

    omo

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,9 @@
 import Star from '../images/Star 1.svg';
-// import Image1 from '../images/image 12.png';
 
-
-function Card() {
+function Card(props) {
     return (
         <div className="card">
-            <img src="../images/card-img-1.png" className="card--image"  alt="card-pic"/>
+            <img src={props.img} className="card--image"  alt="card-pic"/>
             <div className="card--stats">
                 <img src= {Star} className="card--star" alt="card-review" />
                 <span>5.0</span>
