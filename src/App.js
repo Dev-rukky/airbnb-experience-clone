8389f2f0c3aae6d9fcbd3c5d60db6f96253103de
Rukky <abrukevwe@gmail.com>
2023-11-10 21:27:35 +0100

    hmm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './styles/App.css';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Banner />
+      <Card />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,21 @@
-function Card() {
+import Star from '../images/Star 1.svg';
+// import Image1 from '../images/image 12.png';
 
+
+function Card() {
+    return (
+        <div className="card">
+            <img src="../images/card-img-1.png" className="card--image"  alt="card-pic"/>
+            <div className="card--stats">
+                <img src= {Star} className="card--star" alt="card-review" />
+                <span>5.0</span>
+                <span className="gray">(6) • </span>
+                <span className="gray">USA</span>
+            </div>
+            <p>Life Lessons with Katie Zaferes</p>
+            <p><span className="bold">From $136</span> / person</p>
+        </div>
+    )
 }
 
-export default Card
+export default Card;
