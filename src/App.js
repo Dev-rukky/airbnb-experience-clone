9a3c163c2157bc9917ea798925f55c9fe543784c
Rukky <abrukevwe@gmail.com>
2023-11-11 10:09:58 +0100

    omo

--- src/App.js
+++ src/App.js
@@ -2,49 +2,32 @@ import './styles/App.css';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
 import Card from './components/Card';
-import cardImg1 from './images/card-img-1.png';
-import cardImg2 from './images/card-img-2.png';
-import cardImg3 from './images/card-img-3.png';
+import Data from './Data';
 
 function App() {
+  console.log(Data)
 
-  
-  const imgs = {
-    img1: cardImg1,
-    img2: cardImg2,
-    img3: cardImg3
-}
+  const cards = Data.map(item => {
+    return (
+      <Card
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+      />
+    )
+  })
 
   return (
     <div className="App">
       <Navbar />
       <Banner />
-      <Card 
-        img = {imgs.img1}
-        rating = "5.0"
-        reviewCount = {6}
-        country = "England"
-        title = "Life Lessons with Katie Zaferes"
-        price = {136}
-      />
+      <section className='card-list'>
+        {cards}
+      </section>
 
-      <Card 
-        img ={imgs.img2}
-        rating = "5.0"
-        reviewCount = {6}
-        country = "England"
-        title = "Life Lessons with Katie Zaferes"
-        price = {136}
-      />
-
-      <Card 
-        img ={imgs.img3}
-        rating = "5.0"
-        reviewCount = {6}
-        country = "England"
-        title = "Life Lessons with Katie Zaferes"
-        price = {136}
-      />
     </div>
   );
 }
