8f47e9210786d40725669dc5727f152923fbe114
Agbadudu Oghenerukevwe <90446020+Dev-rukky@users.noreply.github.com>
2023-11-16 12:42:17 +0100

    done

--- src/App.js
+++ src/App.js
@@ -10,12 +10,8 @@ function App() {
   const cards = Data.map(item => {
     return (
       <Card
-        img={item.coverImg}
-        rating={item.stats.rating}
-        reviewCount={item.stats.reviewCount}
-        location={item.location}
-        title={item.title}
-        price={item.price}
+        key={item.id}
+        item={item}
       />
     )
   })
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,25 @@
 import Star from '../images/Star 1.svg';
 
 function Card(props) {
+    let badgeText
+
+    if (props.item.OpenSpot === 0) {
+        badgeText = "SOLD OUT"
+    }else if (props.item.location === "Online") {
+        badgeText = "ONLINE"
+    }
     return (
         <div className="card">
-        <img src={props.img} className="card--image" alt='card-pic' />
+        {badgeText && <div className='card-badge'>{badgeText}</div>}
+        <img src={props.item.coverImg} className="card--image" alt='card-pic' />
         <div className="card--stats">
             <img src={Star} className="card--star" alt='rate' />
-            <span>{props.rating}</span>
-            <span className="gray">({props.reviewCount}) • </span>
-            <span className="gray">{props.location}</span>
+            <span>{props.item.stats.rating}</span>
+            <span className="gray">({props.item.stats.reviewCount}) • </span>
+            <span className="gray">{props.item.location}</span>
         </div>
-        <p>{props.title}</p>
-        <p><span className="bold">From ${props.price}</span> / person</p>
+        <p>{props.item.title}</p>
+        <p><span className="bold">From ${props.item.price}</span> / person</p>
     </div>
         
     )
